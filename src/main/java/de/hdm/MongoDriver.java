4452b9d64fccea60c6825c4083ff366e3a3d548a
Luca v. K <luca.v.kannen@gmail.com>
2022-11-06 20:27:11 +0100

    Added functions for driver

--- src/main/java/de/hdm/MongoDriver.java
+++ src/main/java/de/hdm/MongoDriver.java
@@ -3,8 +3,11 @@ import com.mongodb.*;
 import com.mongodb.ServerApi;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
+
+import java.util.function.Consumer;
 
 import static com.mongodb.client.model.Filters.eq;
 //import com.mongodb.client.MongoDatabase;
@@ -16,29 +19,54 @@ public class MongoDriver {
 
     //private static String uri = "mongodb+srv://lv042:<K6Uu9882EFFeWvgU>@dbgrep.o3uj6ms.mongodb.net/?retryWrites=true&w=majority";
 
-    static MongoClientSettings settings;
-    ConnectionString connectionString;
+    static MongoClientSettings settings; //only for testing purposes static
+    static ConnectionString connectionString;
+    static MongoClient mongoClient;
     //Constructor for MongoClient
     public MongoDriver(String url, String user, String password) {
         connectionString = new ConnectionString("mongodb+srv://user:87654321@dbgrep.o3uj6ms.mongodb.net/?retryWrites=true&w=majority");
         settings = MongoClientSettings.builder()
                 .applyConnectionString(connectionString)
                 .serverApi(ServerApi.builder().version(ServerApiVersion.V1).build()).build();
+        mongoClient = MongoClients.create(settings);
     }
     public static void main(String[] args) {
-        MongoClient mongoClient = MongoClients.create(settings);
+        //Own constructor for testing lol
+        connectionString = new ConnectionString("mongodb+srv://user:87654321@dbgrep.o3uj6ms.mongodb.net/?retryWrites=true&w=majority");
+        settings = MongoClientSettings.builder()
+                .applyConnectionString(connectionString)
+                .serverApi(ServerApi.builder().version(ServerApiVersion.V1).build()).build();
+        mongoClient = MongoClients.create(settings);
+
+
         MongoDatabase database = mongoClient.getDatabase("test");
-        database.getCollection("testdb").insertOne(new Document("moive", "MongoDB"));
-        Document movie = new Document("title", "Star Wars")
-                .append("year", 2019)
-                .append("director", "Michael Bay")
-                .append("actors", "Batman");
-        database.getCollection("testdb").insertOne(movie);
-        System.out.println(database.getCollection("testdb").find(eq("title", "Star Wars")).first());
-               // collection.find(eq("title", "Back to the Future")).first();
+        MongoCollection<Document> collection = database.getCollection("testdb");
+        System.out.println("Collection testdb selected successfully");
+
+        //Find object in collection
+        Document myDoc = collection.find(eq("name", "test")).first();
+        System.out.println(myDoc.toJson());
 
 
 
 
     }
+
+    public MongoDatabase getDatabase(String databaseName)  {
+        return mongoClient.getDatabase(databaseName);
+    }
+
+    public void close() {
+        mongoClient.close();
+    }
+
+    public MongoCollection<Document> getCollection(String collectionName, MongoDatabase database) {
+        return database.getCollection(collectionName);
+    }
+
+    public String findFirstKey(String collectionName, MongoDatabase database) {
+        database.getCollection(collectionName).find().first();
+        return null;
+    }
+
 }
