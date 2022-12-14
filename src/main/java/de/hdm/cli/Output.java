75a6aa1ccdae1715bda9f0e9088be2e715cf7754
Heuser <bh070@hdm-stuttgart.de>
2022-12-14 12:22:32 +0100

    delete second main method in output

--- src/main/java/de/hdm/cli/Output.java
+++ src/main/java/de/hdm/cli/Output.java
@@ -6,24 +6,7 @@ import de.hdm.datacontainer.Result;
 
 public class Output {
 
-    public static void main(String[] args) {
-        var s = new String[]{"a", "bb", "ccc", "dddd", "eeeee", "ffffff"};
-
-        var _h = new LinkedHashMap<String, String>();
-        var count = 0;
-        for (String _s : s){
-            _h.put("Column " + count++, _s);
-        }
-        var h = new LinkedHashMap<String, LinkedHashMap<String, String>[]>();
-        LinkedHashMap<String, String>[] _h_arr = new LinkedHashMap[3];
-        _h_arr[0] = _h;
-        _h_arr[1] = _h;
-        _h_arr[2] = _h;
-        h.put("Table 1",_h_arr);
-
-        Result res = new Result(null, null, h);
-        Output.printResult(res);
-    }
+   
 
     public static void printResult(Result result){
         var tableNames = result.getTablenames();
