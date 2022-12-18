773f4beac6967581a43294559fb7185b10db966e
Luca v. K <luca.v.kannen@gmail.com>
2022-12-18 11:48:41 +0100

    Added seperate packages for mongo and sql | Added MongoLogic.java

---
+++ src/main/java/de/hdm/db/mongo/MongoLogic.java
@@ -0,0 +1,28 @@
+package de.hdm.db;
+
+import de.hdm.datacontainer.Query;
+import de.hdm.datacontainer.Result;
+
+public class MongoLogic implements ILogic {
+    
+
+    public SQLConnection sqlConnection;
+
+     
+    public int count(Result result){
+
+        return 0;
+
+    }
+    public int count(String[] result){
+        return 0;
+
+    }
+    public Result request(Query query){
+
+        
+        return null;
+        
+    }
+    
+}
