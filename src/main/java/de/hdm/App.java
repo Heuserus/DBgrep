43c345e72a50ff75d66870d4dff292120e6c0ce2
Luca v. K <luca.v.kannen@gmail.com>
2023-01-21 14:05:28 +0100

    Ascii class now reads from txt file

--- src/main/java/de/hdm/App.java
+++ src/main/java/de/hdm/App.java
@@ -1,12 +1,14 @@
 package de.hdm;
 
 import de.hdm.cli.CLIParser;
+import de.hdm.devutils.AsciiArt;
 import de.hdm.exception.DBGrepException;
 import picocli.*;
 
 public class App {
 
     public static void main(String[] args) {
+        AsciiArt.display_welcome_message();
         CLIParser cliParser = new CLIParser();
         CommandLine cmd = new CommandLine(cliParser);
         int exitCode = -1;
--- src/main/java/de/hdm/devutils/AsciiArt.java
+++ src/main/java/de/hdm/devutils/AsciiArt.java
@@ -9,14 +9,14 @@ import java.nio.file.Paths;
 public class AsciiArt {
 
     public static void display_welcome_message(){
-        System.out.println(" __          __  _                            _          _____  ____   _____ _____  ______ _____  \n" +
-                " \\ \\        / / | |                          | |        |  __ \\|  _ \\ / ____|  __ \\|  ____|  __ \\ \n" +
-                "  \\ \\  /\\  / /__| | ___ ___  _ __ ___   ___  | |_ ___   | |  | | |_) | |  __| |__) | |__  | |__) |\n" +
-                "   \\ \\/  \\/ / _ \\ |/ __/ _ \\| '_ ` _ \\ / _ \\ | __/ _ \\  | |  | |  _ <| | |_ |  _  /|  __| |  ___/ \n" +
-                "    \\  /\\  /  __/ | (_| (_) | | | | | |  __/ | || (_) | | |__| | |_) | |__| | | \\ \\| |____| |     \n" +
-                "     \\/  \\/ \\___|_|\\___\\___/|_| |_| |_|\\___|  \\__\\___/  |_____/|____/ \\_____|_|  \\_\\______|_|     \n" +
-                "                                                                                                  \n" +
-                "                                                                                                  ");
+        String path = "src/main/java/de/hdm/devutils/ascii/Welcome.txt";
+        try {
+            String ascii_art = new String(Files.readAllBytes(Paths.get(path)));
+            System.out.println(ascii_art);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
     //read the ascii art from a file
