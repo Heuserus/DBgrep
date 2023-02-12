e7cfece0b0ab1fccc9c45d3b14c0e97ae5c1b00d
Marco de Jesus António <marco.antonio95@web.de>
2023-02-12 18:49:48 +0100

    #48: improved query with its own preprocessor and changed it to an proper object insted of converting it to a list

--- src/main/java/de/hdm/datacontainer/Query.java
+++ src/main/java/de/hdm/datacontainer/Query.java
@@ -1,33 +1,44 @@
 package de.hdm.datacontainer;
 
+import de.hdm.constants.DBGrepConstants.QueryType;
+import org.apache.commons.collections4.MultiValuedMap;
+import org.apache.commons.collections4.multimap.ArrayListValuedHashMap;
 import picocli.CommandLine.IParameterConsumer;
 import picocli.CommandLine.Model.ArgSpec;
 import picocli.CommandLine.Model.CommandSpec;
-import picocli.CommandLine.Model.OptionSpec;
 import picocli.CommandLine.Option;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 import java.util.Stack;
+import java.util.stream.Collectors;
 
 public class Query {
-    // -c breite >10 <20 -c länge <5
     @Option(names = {"-c",
-            "--column"}, required = false, description = "column to be searched, table must be specified [if used without table search in every column the name provided matches", parameterConsumer = QueryPreprocessor.class)
-    private List<List<String>> columns = new ArrayList<>();
+            "--column"}, description = "column to be searched, table must be specified [if used without table search in every column the name provided matches", parameterConsumer = QueryPreprocessor.class)
+    private MultiValuedMap<String, String> columns = new ArrayListValuedHashMap<>();
+    @Option(names = {"-t", "--table"}, description = "specifies table to be searched")
+    private String table;
 
-    @Option(names = {"-t", "--table"}, required = false, description = "specifies table to be searched", parameterConsumer = Query.QueryPreprocessor.class)
-    private List<String> table = new ArrayList<>();
+    public MultiValuedMap<String, String> getColumns() {
+        return columns;
+    }
 
-    @Option(names = {"-o", "--object"}, required = false, description = "object to be searched")
-    private Optional<String> object;
+    public String getTable() {
+        return table;
+    }
 
-    public List<List<List<String>>> parseQuery() {
-        var queryAsList = new ArrayList<List<List<String>>>();
-        queryAsList.add(List.of(table));
-        queryAsList.add(columns);
-        return queryAsList;
+    public QueryType getQueryType() {
+        final boolean columnsPresent = !columns.isEmpty();
+        final boolean tablePresent = table != null;
+
+        if (tablePresent && columnsPresent)
+            return QueryType.SEARCH_OBJECTS;
+        else if (tablePresent)
+            return QueryType.SEARCH_TABLE_NAMES;
+        else
+            return QueryType.SEARCH_COLUMN_NAMES;
     }
 
     public static class QueryPreprocessor implements IParameterConsumer {
@@ -36,41 +47,42 @@ public class Query {
             if (args.isEmpty()) {
                 return;
             }
-            List<String> optionList = new ArrayList<>();
-            for (Character c : commandSpec.posixOptionsMap().keySet()) {
-                optionList.add("-" + c);
-            }
-            for (String currentOptionFlag : ((OptionSpec) argSpec).names()) {
-                switch (currentOptionFlag) {
-                    case "--column":
-                        currentOptionFlag = currentOptionFlag.substring(0, 2);
-                    case "-c":
-                        consumeColumn(currentOptionFlag, argSpec.getValue(), optionList, args);
-                        break;
-                    case "--table":
-                        currentOptionFlag = currentOptionFlag.substring(0, 2);
-                    case "-t":
-                        consumeTable(currentOptionFlag, argSpec.getValue(), optionList, args);
-                        break;
-                }
-            }
+            var configuredOptions = commandSpec.options()
+                    .stream()
+                    .flatMap(optionSpec -> Arrays.stream(optionSpec.names()))
+                    .collect(Collectors.toList());
+
+            consumeColumn(argSpec.getValue(), configuredOptions, args);
         }
 
-        private void consumeTable(String currentOptionFlag, List<String> table, List<String> optionList, Stack<String> args) {
-            table.add(currentOptionFlag);
+        /**
+         * Consumes columns, their respective names and its conditions of the passed application
+         * arguments by adding them to the {@link Query#columns columns} map of the current
+         * {@link Query query object}. If there are no conditions provided regarding the current
+         * processed column option, an empty map will be added as condition instead.
+         *
+         * @param columns    map of the current query object
+         * @param optionList list of options configured for this application
+         * @param args       arguments passed to this app
+         */
+        private void consumeColumn(MultiValuedMap<String, String> columns, List<String> optionList, Stack<String> args) {
+            if (args.isEmpty()) throw new IllegalArgumentException(); //TODO: throw proper exception
+
+            final String columnName = args.pop();
+            final List<String> columnConditions = new ArrayList<>();
+
             while (!args.isEmpty() && !optionList.contains(args.peek())) {
-                table.add(args.pop());
+//                if (!isValidArgument(currentOptionFlag, args.peek())) {
+//                    throw new IllegalArgumentException(); //TODO: throw proper exception
+//                }
+                columnConditions.add(args.pop());
             }
+            columns.putAll(columnName, columnConditions.isEmpty() ? List.of() : columnConditions);
         }
 
-        private void consumeColumn(String currentOptionFlag, List<List<String>> columns, List<String> optionList, Stack<String> args) {
-            List<String> columnParameters = new ArrayList<>();
-            columnParameters.add(currentOptionFlag);
-
-            while (!args.isEmpty() && !optionList.contains(args.peek())) {
-                columnParameters.add(args.pop());
-            }
-            columns.add(columnParameters);
+        private boolean isValidArgument(final String optionContext, final String argument) {
+            //TODO: to be implemented
+            return false;
         }
     }
 }
