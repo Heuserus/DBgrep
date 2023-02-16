9f756774a3c6f01cc396a3f1baf63b3633785659
Marco de Jesus António <marco.antonio95@web.de>
2023-02-16 01:52:34 +0100

    #48: cleaned up not unused code

--- src/main/java/de/hdm/cli/CLIParser.java
+++ src/main/java/de/hdm/cli/CLIParser.java
@@ -14,41 +14,39 @@ import java.util.concurrent.Callable;
 @Command(name = "dbgrep", mixinStandardHelpOptions = true, version = "dbgrep 0.0.1", description = "offers features to conveniently search thorugh a database")
 public class CLIParser implements Callable<Integer> {
 
-  @ArgGroup(multiplicity = "1", heading = "Connection Details")
-  DBConnectionParser connectionProperties;
+    @ArgGroup(multiplicity = "1", heading = "Connection Details")
+    DBConnectionParser connectionProperties;
 
-  @ArgGroup(exclusive = false, multiplicity = "1..*")
-  List<Query> queryArguments;
+    @ArgGroup(exclusive = false, multiplicity = "1..*")
+    List<Query> queryArguments;
 
-  @Option(names = { "-h", "--help" }, usageHelp = true, description = "display help message")
-  private boolean help = false;
+    @Option(names = {"-h", "--help"}, usageHelp = true, description = "display help message")
+    private boolean help = false;
 
 
-  @Option(names = { "-r", "--recursive" }, description = "follow foreign keys")
-  private boolean recursive;
+    @Option(names = {"-r", "--recursive"}, description = "follow foreign keys")
+    private boolean recursive;
 
-  
-  @Override
-  public Integer call() throws Exception {
-    ConnectionInfo connectionInfo;
-    try {
-      connectionInfo = connectionProperties.parse();
-    } catch (Exception e) {
-      if (e instanceof DBGrepException) {
-        System.err.println("Failed to Parse Profile");
-        DBGrepException exception = (DBGrepException) e;
-        return exception.getExitCode().getCode();
-      }
-      e.printStackTrace();
-      return -1;
+
+    @Override
+    public Integer call() throws Exception {
+        ConnectionInfo connectionInfo;
+        try {
+            connectionInfo = connectionProperties.parse();
+        } catch (Exception e) {
+            if (e instanceof DBGrepException exception) {
+                System.err.println("Failed to Parse Profile");
+                return exception.getExitCode().getCode();
+            }
+            e.printStackTrace();
+            return -1;
+        }
+
+        new Controller(connectionInfo, queryArguments).run();
+        return 0;
     }
 
-    Controller controller = new Controller(connectionInfo, queryArguments);
-    controller.run();
-    return 0;
-  }
-
-  public boolean isHelp() {
-    return help;
-  }
+    public boolean isHelp() {
+        return help;
+    }
 }
--- src/main/java/de/hdm/cli/DBConnectionParser.java
+++ src/main/java/de/hdm/cli/DBConnectionParser.java
@@ -1,50 +1,37 @@
 package de.hdm.cli;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.Optional;
-
-import com.fasterxml.jackson.core.exc.StreamReadException;
-import com.fasterxml.jackson.databind.DatabindException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
-
 import de.hdm.constants.DBGrepConstants.ExitCode;
 import de.hdm.datacontainer.ConnectionInfo;
 import de.hdm.exception.MissingProfileException;
 import picocli.CommandLine.ArgGroup;
 import picocli.CommandLine.Option;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Optional;
+
 public class DBConnectionParser {
     @ArgGroup(exclusive = false, heading = "Connection Properties")
     ConnectionInfo connectionInfo;
 
-    @Option(names = { "-p", " --profile" }, description = "connection path/profile")
+    @Option(names = {"-p", " --profile"}, description = "connection path/profile")
     Optional<String> profile;
 
 
-    public ConnectionInfo parse()
-            throws StreamReadException, DatabindException, IOException, MissingProfileException {
+    public ConnectionInfo parse() throws IOException, MissingProfileException {
         if (connectionInfo != null) {
             return connectionInfo;
         }
 
-        if (!profile.isPresent()) {
+        if (profile.isEmpty()) {
             throw new MissingProfileException(ExitCode.MISSING_PROFILE);
         }
 
         File file = new File(profile.get());
         ObjectMapper objectMapper = new ObjectMapper(new YAMLFactory());
 
-        ConnectionInfo profile = objectMapper.readValue(file, ConnectionInfo.class);
-        return profile;
-    }
-    
-    public ConnectionInfo getConnectionInfo() {
-        return connectionInfo;
-    }
-
-    public Optional<String> getProfile() {
-        return profile;
+        return objectMapper.readValue(file, ConnectionInfo.class);
     }
 }
